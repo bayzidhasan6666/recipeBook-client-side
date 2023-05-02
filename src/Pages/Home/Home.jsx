753f9ebdc22be22c96a60816539d057a7de0a408
bayzidhasan6666 <121633867+bayzidhasan6666@users.noreply.github.com>
2023-05-02 23:59:30 +0800

    added six chefs section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const images = [
   {
@@ -14,8 +14,16 @@ const images = [
     alt: 'Banner Image 3',
   },
 ];
-const Home = () => {
+const Home = (chef) => {
+  const [chefs, setChefs] = useState([]);
   const [activeIndex, setActiveIndex] = useState(0);
+
+  useEffect(() => {
+    fetch('http://localhost:5000/chefs')
+      .then((res) => res.json())
+      .then((data) => setChefs(data))
+      .catch((error) => console.log(error));
+  }, []);
 
   const handleNextClick = () => {
     setActiveIndex((activeIndex + 1) % images.length);
@@ -26,41 +34,99 @@ const Home = () => {
   };
 
   return (
-    <div className="banner-container bg-gray-900 md:flex justify-around container ">
-      <div className="banner-text px-4 py-8 md:w-1/2">
-        <h1 className="text-4xl font-bold text-white mb-4">
-          Welcome to Recipe Book
-        </h1>
-        <p className="text-lg text-gray-300 mb-8">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
-          varius enim in eros elementum tristique.
-        </p>
-        <button className="bg-white text-gray-800 font-bold py-2 px-4 rounded-full hover:bg-gray-300">
-          Learn More
-        </button>
-      </div>
-      <div className="banner-slider relative md:w-1/2">
-        <img
-          src={images[activeIndex].url}
-          alt={images[activeIndex].alt}
-          className="md:w-full lg:11/12 h-96 object-cover rounded-lg"
-        />
-        <div className="slider-controls absolute bottom-0 right-0 mb-4 mr-4">
-          <button
-            className="bg-white text-gray-800 font-bold py-2 px-4 rounded-full hover:bg-gray-300"
-            onClick={handlePrevClick}
-          >
-            Previous
-          </button>
-          <button
-            className="bg-white text-gray-800 font-bold py-2 px-4 rounded-full hover:bg-gray-300 ml-4"
-            onClick={handleNextClick}
-          >
-            Next
+    <>
+      {' '}
+      <div className="banner-container bg-gray-900 md:flex justify-around container ">
+        <div className="banner-text px-4 py-8 md:w-1/2">
+          <h1 className="text-4xl font-bold font-serif text-emerald-400 mb-4">
+            Welcome to Recipe Book
+          </h1>
+          <p className="text-lg text-gray-300 mb-8">
+            With Grand Restaurant theme you can build a website for cafe,
+            bistro, restaurant and other online delivery related business site
+            with ready-made designed page templates and intuitive page builder
+          </p>
+          <button className="bg-red-400 text-gray-900 font-bold py-2 px-4 rounded-full hover:bg-red-500">
+            Learn More
           </button>
         </div>
+        <div className="banner-slider relative md:w-1/2">
+          <img
+            src={images[activeIndex].url}
+            alt={images[activeIndex].alt}
+            className="md:w-full lg:11/12 h-96 object-cover rounded-lg"
+          />
+          <div className="slider-controls absolute bottom-0 right-0 mb-4 mr-4">
+            <button
+              className="bg-white text-gray-800 font-bold py-2 px-4 rounded-full hover:bg-gray-300"
+              onClick={handlePrevClick}
+            >
+              Previous
+            </button>
+            <button
+              className="bg-white text-gray-800 font-bold py-2 px-4 rounded-full hover:bg-gray-300 ml-4"
+              onClick={handleNextClick}
+            >
+              Next
+            </button>
+          </div>
+        </div>
       </div>
-    </div>
+      <h1 className="text-center text-emerald-400 text-4xl font-serif py-10">
+        Most Famous Chefs
+      </h1>
+      {chefs.map((chef) => (
+        <div key={chef.id}>
+          {' '}
+          <section className="banner bg-gray-900 py-12">
+            <div className="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+              <div className="lg:flex lg:items-center lg:justify-between">
+                <div className="flex-1 min-w-0">
+                  <img
+                    className="h-44 w-44 rounded-full object-cover "
+                    src={chef.chefPicture}
+                    alt="Chef Picture"
+                  />
+                </div>
+                <div className="mt-8 lg:mt-0 lg:ml-8">
+                  <h1 className="text-3xl font-extrabold text-red-500 sm:text-4xl font-serif">
+                    {chef.chefName}
+                  </h1>
+                  <p className="mt-3 max-w-2xl text-lg text-gray-500">
+                    {chef.bio}
+                  </p>
+                  <div className="mt-6 flex flex-wrap">
+                    <div className="flex items-center mr-6">
+                      <span className="text-2xl font-bold text-gray-600">
+                        {chef.likes}
+                      </span>
+                      <span className="ml-2 text-gray-500">Likes</span>
+                    </div>
+                    <div className="flex items-center mr-6">
+                      <span className="text-2xl font-bold text-gray-500">
+                        {chef.yearsOfExperience}
+                      </span>
+                      <span className="ml-2 text-gray-500">
+                        Years of Experience
+                      </span>
+                    </div>
+                    <div className="flex items-center">
+                      <span className="text-2xl font-bold text-gray-500">
+                        {chef.numberOfRecipes}
+                      </span>
+                      <span className="ml-2 text-gray-400">
+                        Number of Recipes
+                      </span>
+                      <button className="btn ml-5">View Recipe</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </section>
+        </div>
+      ))}
+    </>
   );
 };
 
--- src/Pages/Shared/Header/Header.jsx
+++ src/Pages/Shared/Header/Header.jsx
@@ -2,7 +2,7 @@ import React, { useContext } from 'react';
 import { NavLink } from 'react-router-dom';
 import { AuthContext } from '../../../Providers/AuthProvider';
 
-const Header = () => {
+const Header = (props) => {
   const { user, logOut } = useContext(AuthContext);
   console.log(user);
 
@@ -12,65 +12,70 @@ const Header = () => {
       .catch((error) => console.log(error));
   };
   return (
-    <nav className="flex items-center justify-between flex-wrap bg-gray-800 shadow-xl p-5 ">
-      <div className="flex items-center flex-shrink-0 text-white mr-6">
-        <NavLink
-          to="/"
-          className="font-semibold italic text-3xl tracking-tight text-red-400 font-serif"
-        >
-          Recipe Book
-        </NavLink>
-      </div>
-      <div className="flex-grow block w-full md:flex md:items-center md:w-auto">
-        <div className="text-sm md:flex-grow">
+    <div>
+      {' '}
+      <nav className="flex items-center justify-between flex-wrap bg-gray-800 shadow-xl p-5 ">
+        <div className="flex items-center flex-shrink-0 text-white mr-6">
           <NavLink
             to="/"
-            className="block mt-4 md:inline-block md:mt-0 text-white hover:text-gray-200 mr-4"
+            className="font-semibold italic text-2xl btn tracking-tight text-red-400 font-serif capitalize "
           >
-            Home
-          </NavLink>
-          <NavLink
-            to="/blog"
-            className="block mt-4 md:inline-block md:mt-0 text-white hover:text-gray-200 mr-4"
-          >
-            Blog
+            Recipe Book
           </NavLink>
         </div>
-        <div>
-          {user ? (
-            <div className="flex items-center">
-              <img
-                className="inline-block h-9 w-9 rounded-full cursor-pointer"
-                src={
-                  user.photoURL
-                    ? user.photoURL
-                    : 'https://e7.pngegg.com/pngimages/81/570/png-clipart-profile-logo-computer-icons-user-user-blue-heroes-thumbnail.png'
-                }
-                alt={user.displayName}
-                title={
-                  user.displayName
-                    ? user.displayName
-                    : 'User Name Not Available'
-                }
-              />
-              <button
-                onClick={handleLogOut}
-                className="ml-4 hover:bg-red-400 text-red-400 font-semibold py-2 px-4 border border-red-400 rounded shadow hover:text-white"
-              >
-                Logout
-              </button>
-            </div>
-          ) : (
+        <div className="flex-grow block w-full md:flex md:items-center md:w-auto">
+          <div className="text-sm md:flex-grow">
             <NavLink
-              to="/login"
-              className="inline-block text-sm px-4 py-2 leading-none border rounded text-green-500 border-white hover:border-transparent hover:text-white hover:bg-green-400 mt-4 md:mt-0"
+              to="/"
+              className="block mt-4 md:inline-block md:mt-0 text-white hover:text-gray-200 mr-4"
             >
-              Login
+              Home
             </NavLink>
-          )}
+            <NavLink
+              to="/blog"
+              className="block mt-4 md:inline-block md:mt-0 text-white hover:text-gray-200 mr-4"
+            >
+              Blog
+            </NavLink>
+          </div>
+          <div>
+            {user ? (
+              <div className="flex items-center">
+                <img
+                  className="inline-block h-9 w-9 rounded-full cursor-pointer"
+                  src={
+                    user.photoURL
+                      ? user.photoURL
+                      : 'https://e7.pngegg.com/pngimages/81/570/png-clipart-profile-logo-computer-icons-user-user-blue-heroes-thumbnail.png'
+                  }
+                  alt={user.displayName}
+                  title={
+                    user.displayName
+                      ? user.displayName
+                      : 'User Name Not Be Available'
+                  }
+                />
+                <button
+                  onClick={handleLogOut}
+                  className="ml-4 hover:bg-red-400 text-red-400 font-semibold py-2 px-4 border border-red-400 rounded shadow hover:text-white"
+                >
+                  Logout
+                </button>
+              </div>
+            ) : (
+              <NavLink
+                to="/login"
+                className="inline-block text-sm px-4 py-2 leading-none border rounded text-green-500 border-white hover:border-transparent hover:text-white hover:bg-green-400 mt-4 md:mt-0"
+              >
+                Login
+              </NavLink>
+            )}
+          </div>
         </div>
-      </div>
-    </nav>
+      </nav>
+
+      
+    </div>
   );
 };
 
